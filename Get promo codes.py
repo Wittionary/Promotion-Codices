cf4ab0a1a81d92a101c2a74972d09a6f3516870e
Witt Allen <qwert59@gmail.com>
2017-05-05 15:36:36 -0500

    - Grabbing text from page successfully - Promos are in array - TODO: Iterate through array and print results - TODO: Design database schema?, learn how to store to database

--- Get promo codes.py
+++ Get promo codes.py
@@ -1,5 +1,31 @@
 from urllib.request import urlopen as uRequest
 from bs4 import BeautifulSoup as soup
+import urllib.request
+
+#http://stackoverflow.com/questions/16627227/http-error-403-in-python-3-web-scraping
+class AppURLopener(urllib.request.FancyURLopener):
+    version = "Mozilla/5.0"
+uOpener = AppURLopener()
 
 my_url = 'https://www.relay.fm/connected/140'
-uClient = uRequest(my_url) #throws 403 error, adjust HTML user-agent per http://stackoverflow.com/questions/16627227/http-error-403-in-python-3-web-scraping
+
+# Opens the connection, grabs the page
+uClient = uOpener.open(my_url) 
+page_html = uClient.read()
+
+# Closes connection
+uClient.close()
+
+# HTML parsing
+page_soup = soup(page_html, "html.parser")
+
+# Grabs all (1) promo div elements with class of "sp-area"
+sp_areas = page_soup.findAll("div", {"class":"sp-area"}) # This is an array
+sp_areas[0].ul.li.a["href"] # 'http://casper.com/connected'
+sp_areas[0].ul.li.a.text # 'Casper'
+
+promo = sp_areas[0].findAll("li") # This is an array
+
+# TODO: Iterate through array 'promo' and print results
+# promo[0].text
+# promo[0].a["href"]
