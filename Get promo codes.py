27aba5fa97442c3588d1bd318408d283dcad83e6
Witt Allen <qwert59@gmail.com>
2018-08-02 21:51:45 -0500

    Can run from start to finish w/o error. Next: Loop through shows and read them all instead of hardcoded ".../connected/202"

--- Get promo codes.py
+++ Get promo codes.py
@@ -1,4 +1,4 @@
-# Following the tutorial at: 
+# Following the tutorial at:
 # https://www.youtube.com/watch?v=XQgXKtPSzUI
 from urllib.request import urlopen as uRequest
 from bs4 import BeautifulSoup as soup
@@ -8,17 +8,18 @@ import urllib.request
 # - Filter out RETIRED shows from ACTIVE ones
 # - Filter duplicate promos out removing the oldest codes first
 
-#http://stackoverflow.com/questions/16627227/http-error-403-in-python-3-web-scraping
+# http://stackoverflow.com/questions/16627227/http-error-403-in-python-3-web-scraping
 class AppURLopener(urllib.request.FancyURLopener):
     version = "Mozilla/5.0"
 
-uOpener = AppURLopener()
 
+uOpener = AppURLopener()
+root_url = "https://www.relay.fm"
 # --------------- List of podcasts ---------------
-showlist_url = 'https://www.relay.fm/shows'
+showlist_url = "https://www.relay.fm/shows"
 
 # Opens the connection, grabs the page
-uClient = uOpener.open(showlist_url) 
+uClient = uOpener.open(showlist_url)
 showlist_html = uClient.read()
 
 # Closes connection
@@ -28,29 +29,29 @@ uClient.close()
 showlist_soup = soup(showlist_html, "html.parser")
 
 # Grabs both active and retired shows
-podcasts = showlist_soup.findAll("h3", {"class":"broadcast__name"})
+podcasts = showlist_soup.findAll("h3", {"class": "broadcast__name"})
 numOfPodcasts = len(podcasts)
 podcastURL = []
 # podcasts[1].a.text # 'Analog(ue)'
-# podcasts[1].a["href"] # '\analogue'
-
+# podcasts[1].a["href"] # '/analogue'
 # https://docs.python.org/3/tutorial/errors.html
 # Make dictionary of all the podcast names, partial URLs, and full URLs
 podcastURL = []
 podcastText = []
 
 for podcast in podcasts:
-	try:
-		podcastURL.append(podcast.a["href"])
-		podcastText.append(podcast.a.text)
-	except TypeError: # This was being thrown by Master Feed since there's no URL to show
-		pass
+    try:
+        podcastURL.append(podcast.a["href"])
+        podcastText.append(podcast.a.text)
+    except TypeError:  # This was being thrown by Master Feed since there's no URL to show
+        pass
 
+print("There are " + str(len(podcastURL)) + " podcasts.")
 # --------------- Get five most recent episodes ---------------
-show_url = 'https://www.relay.fm/connected'
+show_url = "https://www.relay.fm/connected"
 
 # Opens the connection, grabs the page
-uClient = uOpener.open(show_url) 
+uClient = uOpener.open(show_url)
 show_html = uClient.read()
 
 # Closes connection
@@ -58,32 +59,26 @@ uClient.close()
 
 # HTML parsing
 show_soup = soup(show_html, "html.parser")
-episode_wrap = show_soup.findAll("div", {"class":"episode-wrap animated"})
+episode_wrap = show_soup.findAll("div", {"class": "episode-wrap animated"})
 
 episode_num = []
 episode_title = []
 
 for episode in episode_wrap[:5]:
-	episodes = episode_wrap.h3.a.text
-	episodes = episodes.split(":")
-	episode_num.append(episodes.strip("#"))
-	episode_title.append(episodes.strip())
-
-for episode in episode_wrap[:5]:
-	episodes = episode.h3.a.text
-	episodes = episodes.split(":")
-	episode_num.append(episodes[0].strip("#"))
-	episode_title.append(episodes[1].strip())
+    episodes = episode.h3.a.text
+    episodes = episodes.split(":")
+    episode_num.append(episodes[0].strip("#"))
+    episode_title.append(episodes[1].strip())
 
 for x in range(len(episode_num)):
-	print("Episode " + episode_num[x] + " is titled \'" + episode_title[x] + "\'")
+    print("Episode " + episode_num[x] + " is titled '" + episode_title[x] + "'")
 
 # --------------- Episode page ---------------
 # TODO: Iterate through 5 most recent shows; compare with today's date and only grab stuff in the past 3 months?
-episode_url = 'https://www.relay.fm/connected/202'
+episode_url = "https://www.relay.fm/connected/202"
 
 # Opens the connection, grabs the page
-uClient = uOpener.open(episode_url) 
+uClient = uOpener.open(episode_url)
 episode_html = uClient.read()
 
 # Closes connection
@@ -93,20 +88,21 @@ uClient.close()
 episode_soup = soup(episode_html, "html.parser")
 
 # Grabs all (1) promo div elements with class of "sp-area"
-sp_areas = episode_soup.findAll("div", {"class":"sp-area"}) # This is an array
-sp_areas[0].ul.li.a["href"] # 'https://smilesoftware.com/'
-sp_areas[0].ul.li.a.text # 'TextExpander'
+sp_areas = episode_soup.findAll("div", {"class": "sp-area"})  # This is an array
+sp_areas[0].ul.li.a["href"]  # 'https://smilesoftware.com/'
+sp_areas[0].ul.li.a.text  # 'TextExpander'
 
-promos = sp_areas[0].findAll("li") # This is an array
+promos = sp_areas[0].findAll("li")  # This is an array
 
 # https://www.tutorialspoint.com/python/python_for_loop.htm
 for promo in promos:
-	promo.a["href"]
-	promo.text
+    promo.a["href"]
+    promo.text
 
 # Gets publish date of episode
-pubdates = episode_soup.findAll("p", {"class":"pubdate"})
-pubdate = pubdates[0].small.text.split('·')
-print(pubdate[0].strip('\n'))
+pubdates = episode_soup.find("p", {"class": "pubdate"})
+pubdate = pubdates.small.text.split("·")
+pubdate = pubdate[0].strip("\n")
+print(pubdate)
 
-# TODO: format date so that it's mm-dd-yyyy and 
+# TODO: format date so that it's mm-dd-yyyy and
